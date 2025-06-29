87f6c6abc7dffcc1bd6e69734f2f93a60caa716f
vessipetrova <135272277+vessipetrova@users.noreply.github.com>
2025-06-29 10:27:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.getElementById('apply-btn').addEventListener('click', function() {
   var color = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = color;
 });
+
+document.getElementById('random-btn').addEventListener('click', function() {
+  var color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+  document.getElementById('box').style.backgroundColor = color;
+});
